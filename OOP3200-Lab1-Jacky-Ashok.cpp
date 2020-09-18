ac66bb1051635fe6e50b0c4924b3cce9f7c00e15
Jacky <Jacky@PC>
2020-09-18 15:48:53 -0400

    work ticket class created

--- OOP3200-Lab1-Jacky-Ashok.cpp
+++ OOP3200-Lab1-Jacky-Ashok.cpp
@@ -18,86 +18,126 @@
 
 using namespace std;
 
-const int DAY_MONTH_YEAR = 2;
 
- class WorkTicket
- {
- public:
-//constructor
-WorkTicket(int ticketNumber = 0, string clientID = " ", int workTicketDate[DAY_MONTH_YEAR] = {1, 1, 2000}, string issueDescription = " ");
-
-// Accessors:
-//get the work ticket number
-int GetWorkTicketNumber();
-//get the client ID
-string GetClientID();
-//get the work ticket date
-int GetWorkTicketDate();
-//get the clients problem description
-string GetDescription();
+class WorkTicket
+{
+public:
+    //constructor
+    WorkTicket(int ticketNumber = 0, string clientID = " ", int workTicketDay = 1, int workTicketMonth = 1, int workTicketYear = 2000, string issueDescription = " ");
 
 
-//Mutators:
-// set ticket number
-void SetWorkTicketNumber(int ticketNumber);
-// set client ID
-void SetClientID(string clientID);
-// set work ticket Date
-void SetWorkTicketDate(int workTicketDate[DAY_MONTH_YEAR]);
-// set issue Description
-void SetIssueDescription(string issueDescription);
-// set work ticket
-void SetWorkTicket(int ticketNumber, string clientID, int workTicketDate[DAY_MONTH_YEAR], string issueDescription);
+    // Accessors:
+    //get the work ticket number
+    int GetWorkTicketNumber();
+    //get the client ID
+    string GetClientID();
+    //get the work ticket date
+    int GetWorkTicketDay();
+    //get the work ticket month
+    int GetWorkTicketMonth();
+    //get the work ticket year
+    int GetWorkTicketYear();
+    //get the clients problem description
+    string GetDescription();
 
 
- private:
-   int ticketNumber; //work ticket number variable
-   string clientID; // client ID variable
-   int workTicketDate[DAY_MONTH_YEAR];
-   string issueDescription; //issue description variable
+    //Mutators:
+    // set ticket number
+    void SetWorkTicketNumber(int ticketNumber);
+    // set client ID
+    void SetClientID(string clientID);
+    // set work ticket Date
+    void SetWorkTicketDay(int workTicketDay);
+    // set work ticket Month
+    void SetWorkTicketMonth(int workTicketMonth);
+    // set work ticket Year
+    void SetWorkTicketYear(int workTicketYear);
+    // set issue Description
+    void SetIssueDescription(string issueDescription);
+    // set work ticket
+    void SetWorkTicket(int ticketNumber, string clientID, int workTicketDay, int workTicketMonth, int workTicketYear, string issueDescription);
 
- };
+
+private:
+    int ticketNumber; //work ticket number variable
+    string clientID; // client ID variable
+    int workTicketDay;//work ticket day variable
+    int workTicketMonth;//work ticket month variable
+    int workTicketYear;//work ticket year variable
+    string issueDescription; //issue description variable
+
+};
 
 
 int main()
 {
-//user inputted variables
-int inputtedticketNumber;
-string inputtedClientID;
-int inputtedworkTicketDate[DAY_MONTH_YEAR];
-string inputtedissueDescription;
+    //user inputted variables
+    int inputtedTicketNumber;
+    string inputtedClientID;
+    int inputtedWorkTicketDay;
+    int inputtedWorkTicketMonth;
+    int inputtedWorkTicketYear;
+    string inputtedIssueDescription;
 
 }
 
 
 
-WorkTicket::WorkTicket(int ticketNumber, string clientID, int workTicketDate[DAY_MONTH_YEAR], string issueDescription)
+WorkTicket::WorkTicket(int inputtedTicketNumber, string inputtedClientID, int inputtedWorkTicketDay, int inputtedWorkTicketMonth, int inputtedWorkTicketYear, string inputtedIssueDescription)
 {
-  SetWorkTicket(int ticketNumber, string clientID, int workTicketDate[DAY_MONTH_YEAR], string issueDescription);
+    SetWorkTicket(inputtedTicketNumber, inputtedClientID, inputtedWorkTicketDay, inputtedWorkTicketMonth, inputtedWorkTicketYear, inputtedIssueDescription);
 }
 
-void WorkTicket::SetWorkTicket(int ticketNumber, string clientID, int workTicketDate[DAY_MONTH_YEAR], string issueDescription)
+void WorkTicket::SetWorkTicket(int inputtedTicketNumber, string inputtedClientID, int inputtedWorkTicketDay, int inputtedWorkTicketMonth, int inputtedWorkTicketYear, string inputtedIssueDescription)
 {
-  SetWorkTicketNumber(int ticketNumber);
-  // set client ID
-  SetClientID(string clientID);
-  // set work ticket Date
-  SetWorkTicketDate(int workTicketDate[DAY_MONTH_YEAR]);
-  // set issue Description
-  SetIssueDescription(string issueDescription);
+    SetWorkTicketNumber(inputtedTicketNumber);
+    // set client ID
+    SetClientID(inputtedClientID);
+    // set work ticket Day
+    SetWorkTicketDay(inputtedWorkTicketDay);
+    // set work ticket Month
+    SetWorkTicketDay(inputtedWorkTicketMonth);
+    // set work ticket Year
+    SetWorkTicketDay(inputtedWorkTicketYear);
+    // set issue Description
+    SetIssueDescription(inputtedIssueDescription);
 }
 
-void WorkTicket::SetWorkTicketNumber(int ticketNumber)
+//setter for the work ticket number
+void WorkTicket::SetWorkTicketNumber(int inputtedTicketNumber)
 {
-  ticketNumber = inputtedticketNumber;
+    ticketNumber = inputtedTicketNumber;
 }
 
-
-void WorkTicket::SetClientID(string clientID)
+//setter for the client ID
+void WorkTicket::SetClientID(string inputtedClientID)
 {
-  
+    clientID = inputtedClientID;
 }
 
+//setter for workTicketDay
+void WorkTicket::SetWorkTicketDay(int inputtedWorkTicketDay)
+{
+    workTicketDay = inputtedWorkTicketDay;
+}
+
+//setter for the work ticket month
+void WorkTicket::SetWorkTicketMonth(int inputtedWorkTicketMonth)
+{
+    workTicketMonth = inputtedWorkTicketMonth;
+}
+
+//setter for the work ticket year
+void WorkTicket::SetWorkTicketYear(int inputtedWorkTicketYear)
+{
+    workTicketYear = inputtedWorkTicketYear;
+}
+
+//setter for the work ticket description
+void WorkTicket::SetIssueDescription(string inputtedIssueDescription)
+{
+    issueDescription = inputtedIssueDescription;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
